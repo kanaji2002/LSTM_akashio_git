0d8633ebbba84ef516da40f6e81907b6fdbed3fc
13-kanaji <st23405@kagawa.kosen-ac.jp>
2023-11-06 15:24:13 +0900

    j

--- web-app/todoapp/urls.py
+++ web-app/todoapp/urls.py
@@ -4,13 +4,13 @@ from .views import TaskCreate, TaskDelete,TaskList,TaskDetail,RegisterTodoApp,Ta
 from django.contrib.auth.views import LogoutView
 urlpatterns=[
     path("", TaskList.as_view(),name="tasks"),
+    #path("", views.main, name='main'),
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
     path("create-task/", TaskCreate.as_view(), name="create-task"),
     path("edit-task/<int:pk>/", TaskUpdate.as_view(), name="edit-task"),
     path("delete-task/<int:pk>/", TaskDelete.as_view(), name="delete-task"),
-    #path("login/", TaskListLoginView.as_view(), name="login"),
-    path("login/", TaskListLoginView.as_view(next_page="main"), name="login"),
+    path("login/", TaskListLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("register/", RegisterTodoApp.as_view(), name="register"),
-     path('', views.main, name='main'),
+    
 ]
