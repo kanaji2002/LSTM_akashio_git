a52d200302ed21ea6e2f066765b3796b60dc556d
kanaji <gellunika6478@icloud.com>
2024-04-01 13:46:35 +0900

    newdata

--- data/gomi.py
+++ data/gomi.py
@@ -1,36 +1,25 @@
 import pandas as pd
-import numpy as np
 
-dfs=[]
-#CSVファイルの読み込み
+# HIU_data_all_data.csvを読み込む
+hiu_data = pd.read_csv('data/edited_akashio_data/HIU_data_all_data.csv')
 
-file1 = f'data/edited_akashio_data/HIU_data_all_data.csv'
-file2 = f'kousuiryou/kousuiryou.csv'
-file3 = f'tyouryuu/get_uv_250m_csv/merge.csv'
-df1 = pd.read_csv(file1, encoding='shift_jis')
-df2 = pd.read_csv(file2, encoding='shift_jis')
-df_t = pd.read_csv(file3, encoding='shift_jis')
+# 降水量のCSVファイルを読み込む（仮のファイル名としてprecipitation.csvを想定）
+precipitation_data = pd.read_csv('kousuiryou/kousuiryou.csv')
 
-# 日付と時間を結合してdatetime列を作成
-df1['datetime'] = pd.to_datetime(df1['datetime'])
-df2['datetime'] = pd.to_datetime(df2['datetime'])
-df_t['datetime'] = pd.to_datetime(df_t['datetime'])
+# 日付と時刻の列をdatetime型に変換
+hiu_data['datetime'] = pd.to_datetime(hiu_data['datetime'])
 
-# datetime列を使って結合する
-merged_df = pd.merge(df1, df2, on='datetime')
+# 降水量データの日付と時刻の列をdatetime型に変換
+precipitation_data['datetime'] = pd.to_datetime(precipitation_data['datetime'])
 
-# df1のdatetime列を日付型に変換
-merged_df['datetime'] = pd.to_datetime(df1['datetime'])
+# 日付を1日ずらす
+precipitation_data['datetime'] = precipitation_data['datetime'] + pd.DateOffset(days=1)
 
-# 日付と時間を分割してdatetime列を作成
-df_t[['date', 'hour', 'time']] = df_t['datetime'].str.split(' ', expand=True)
-df_t['hour'] = df_t['hour'].str.split(':').str[0]  # 時刻の部分を取り除く
-df_t['hour'] = df_t['hour'].astype(int)  # hour列のデータ型を整数型に変換
-df_t['datetime'] = pd.to_datetime(df_t['date'] + ' ' + df_t['hour'].astype(str) + ':00')
+# HIU_data_all_data.csvの日付と時刻をキーにして降水量データをマージ
+hiu_data = pd.merge(hiu_data, precipitation_data, left_on='datetime', right_on='datetime', how='left', suffixes=('', '_1pre'))
 
+# 'kousuiryou_1pre'という列名に変更
+hiu_data.rename(columns={'precipitation': 'kousuiryou_1pre'}, inplace=True)
 
-# 結合
-merged_df = pd.merge(merged_df, df_t, on=['datetime', 'hour'])
-
-# 結合したデータを表示
-print(merged_df)
+# 変更を保存する
+hiu_data.to_csv('data/edited_akashio_data/HIU_data_all_data2.csv', index=False)
