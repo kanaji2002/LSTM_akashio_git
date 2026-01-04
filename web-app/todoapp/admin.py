6fdc68feff3a9a214c0eb96219fc9894ec2252eb
kanaji2002 <115946407+kanaji2002@users.noreply.github.com>
2026-01-04 16:47:29 +0900

    Delete web-app directory

--- web-app/todoapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from todoapp.models import Task
-
-admin.site.register(Task)
--- web-app/todoapp/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Task
-
-
-class TaskModelForm(forms.ModelForm):
-    class Meta:
-        model = Task
-        fields = "__all__"
